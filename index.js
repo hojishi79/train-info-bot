e1e8f5a7c8ad77a19548f85048cadcbf22102ee2
hojishi79 <hojishi79@gmail.com>
2016-06-29 23:32:01 +0900

    詳細ページリンクを取得

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
+"use strict";
+
 var client = require("cheerio-httpcli");
 
 console.log("start");
+
 
 client.fetch("http://transit.yahoo.co.jp/traininfo/area/4/")
     .then(function(result) {
         var $ = result.$;
         var tr = result.$("#mdStatusTroubleLine .elmTblLstLine.trouble table tr");
+        var detailUrls = [];
         tr.each(function() {
             var url = $(this).find("a").attr("href");
             if (url != null) {
-                console.log(url);
+                detailUrls.push(url);
             }
-        })
+        });
+        return detailUrls;
+    })
+    .then(function(detailUrls) {
+        console.log(detailUrls);
     })
     .catch(function(err) {
         console.log(err);
