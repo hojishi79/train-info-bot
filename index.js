84906c5895034d49a9124404718c9b1093627dcd
hojishi79 <hojishi79@gmail.com>
2016-07-08 00:53:55 +0900

    slackへpostする処理を実装

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 "use strict";
 
 var client = require("cheerio-httpcli");
+var rp = require("request-promise");
 
-console.log("start");
-
+var statusColorMap = {
+    "平常運転": "#2BED1A",
+    "列車遅延": "#FFA630",
+    "運転見合わせ": "#E30000",
+    "交通障害情報": "#FFCF59",
+    "その他": "#C224E0"
+};
 
 client.fetch("http://transit.yahoo.co.jp/traininfo/area/4/")
     .then(function(result) {
         var $ = result.$;
         var tr = $("#mdStatusTroubleLine .elmTblLstLine.trouble table tr");
         var details = [];
-        //details.push(client.fetch("http://transit.yahoo.co.jp/traininfo/detail/40/0/"));
         tr.each(function() {
             var url = $(this).find("a").attr("href");
             if (url != null) {
@@ -20,6 +25,7 @@ client.fetch("http://transit.yahoo.co.jp/traininfo/area/4/")
         return Promise.all(details);
     })
     .then(function(results) {
+        var trainInfos = [];
         results.forEach(function(result) {
             var $ = result.$;
             var trainInfo = {
@@ -29,8 +35,40 @@ client.fetch("http://transit.yahoo.co.jp/traininfo/area/4/")
                 status: $("#mdServiceStatus dt").text().slice(4),
                 body: $("#mdServiceStatus dd p").text()
             }
-            console.log(trainInfo);
+            trainInfos.push(trainInfo);
         });
+
+        var attachments = [];
+        trainInfos.forEach(function(info) {
+            var attachment = {
+                "fallback": "<" + info.url + "|" + info.line + " [" + info.status + "]>",
+                "title": info.line,
+                "title_link": info.url,
+                "color": statusColorMap[info.status] == null ? "#9626F5" : statusColorMap[info.status],
+                "fields": [{
+                    "title": info.status,
+                    "value": info.body,
+                    "short": false
+                }]
+            }
+            attachments.push(attachment);
+        });
+
+        var option = {
+            method: "POST",
+            uri: "https://hooks.slack.com/services/XXXXXXXXX/YYYYYYYYY/ZZZZZZZZZZZZZZZZZZZZZZZZ",
+            body: {
+                "username": "train-info-bot",
+                "icon_emoji": ":train:",
+                "text": "列車情報をお届けするぜーぃ！",
+                "attachments": attachments
+            },
+            json: true
+        };
+        return rp(option);
+    })
+    .then(function(res) {
+        console.log(res);
     })
     .catch(function(err) {
         console.log(err);
